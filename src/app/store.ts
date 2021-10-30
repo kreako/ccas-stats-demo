9a06658571d2f0aff0760334810494eaa73fb454
kreako <olivier@kreako.fr>
2021-10-30 22:35:48 +0200

    Use event slice in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import eventReducer from '../features/event/eventSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    event: eventReducer,
   },
 });
 
