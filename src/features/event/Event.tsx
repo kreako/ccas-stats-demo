ebb7cff9d3072acac4daffb845aac6c793d4d7af
kreako <olivier@kreako.fr>
2021-10-31 20:39:50 +0100

    Use CenterAlign component for event page

--- src/features/event/Event.tsx
+++ src/features/event/Event.tsx
@@ -122,12 +122,10 @@ function EventList() {
 
 export default function EventPage() {
   return (
-    <div className="flex">
-      <div className="flex-grow"></div>
+      <CenterAlign>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 flex-grow-0 gap-8">
         <EventList />
       </div>
-      <div className="flex-grow"></div>
-    </div>
+      </CenterAlign>
   )
 }
