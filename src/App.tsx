249b12b9021dd6dd4d19534925512529512e6e11
kreako <olivier@kreako.fr>
2021-10-31 17:45:49 +0100

    Event page as home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,14 @@
-import { useState } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import EventPage from "./features/event/Event"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-blue-500">Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <EventPage />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
-
-export default App
