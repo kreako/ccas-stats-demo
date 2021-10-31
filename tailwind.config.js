a43e5e10aa65f4776e766eb3fbb71645af1cfc78
kreako <olivier@kreako.fr>
2021-10-31 17:32:38 +0100

    Import the full tailwind palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,37 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   mode: "jit",
   purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      // Build your palette here
+      transparent: "transparent",
+      current: "currentColor",
+      gray: colors.blueGray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
