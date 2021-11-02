33a5a3e1be50daf1a8c9eccbdfcab1d6387f576f
kreako <olivier@kreako.fr>
2021-11-02 01:51:08 +0100

    Remove dependencies from city in eventSlice

--- src/features/event/eventSlice.ts
+++ src/features/event/eventSlice.ts
@@ -6,7 +6,6 @@ import {
 } from "@reduxjs/toolkit"
 import { RootState } from "../../app/store"
 import { sub } from "date-fns"
-import { CITIES } from "./city"
 
 export type EventKind = "passage" | "mail" | "phone"
 export type EventGender = "male" | "female" | "x"
@@ -36,29 +35,32 @@ type InitialStateExtra = {
 const randInt = (max: number) => Math.floor(Math.random() * max)
 const randChoices = (choices: Array<any>) => choices[randInt(choices.length)]
 
-export const generateEvent = createAsyncThunk("event/generate", async () => {
-  const events: Array<EventType> = []
-  for (let i = 0; i < 30; i++) {
-    events.push({
-      id: nanoid(),
-      kind: randChoices(["passage", "mail", "phone"]),
-      gender: randChoices(["male", "female", "x"]),
-      age: randChoices([
-        "0-14",
-        "15-24",
-        "25-34",
-        "35-44",
-        "45-54",
-        "55-64",
-        "65-74",
-        "75-+",
-      ]),
-      city: randChoices(Object.keys(CITIES.cities)),
-      date: sub(new Date(), { minutes: randInt(60 * 24 * 7) }).toISOString(),
-    })
+export const generateEvent = createAsyncThunk(
+  "event/generate",
+  async (cityIds: Array<string>) => {
+    const events: Array<EventType> = []
+    for (let i = 0; i < 30; i++) {
+      events.push({
+        id: nanoid(),
+        kind: randChoices(["passage", "mail", "phone"]),
+        gender: randChoices(["male", "female", "x"]),
+        age: randChoices([
+          "0-14",
+          "15-24",
+          "25-34",
+          "35-44",
+          "45-54",
+          "55-64",
+          "65-74",
+          "75-+",
+        ]),
+        city: randChoices(cityIds),
+        date: sub(new Date(), { minutes: randInt(60 * 24 * 7) }).toISOString(),
+      })
+    }
+    return events
   }
-  return events
-})
+)
 
 const eventsAdapter = createEntityAdapter<EventType>({
   // Keep the "all IDs" array sorted based on date (desc)
