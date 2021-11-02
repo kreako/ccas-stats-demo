6e84bda0ae23c43aebe14c83bb933b30f3f1467c
kreako <olivier@kreako.fr>
2021-11-02 21:43:02 +0100

    Add NavBar to App and fix Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import EventPage from "./features/event/Event"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import NavBar from "./ui/NavBar"
+import StatPage from "./features/stat/Stat"
 
 export default function App() {
   return (
     <Router>
+      <NavBar />
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <EventPage />
+        </Route>
+        <Route path="/stats">
+          <StatPage />
         </Route>
       </Switch>
     </Router>
