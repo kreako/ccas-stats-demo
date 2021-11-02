5014bbee95441076c506a0e22a1a69428a6b5d41
kreako <olivier@kreako.fr>
2021-11-02 21:42:42 +0100

    Add a statistics page

---
+++ src/features/stat/Stat.tsx
@@ -0,0 +1,16 @@
+import CenterAlign from "../../ui/CenterAlign"
+
+export default function StatPage() {
+  return (
+    <div className="mt-8">
+      <CenterAlign>
+        <div className="uppercase tracking-wider text-xs font-bold text-sky-900">
+          Les statistiques
+        </div>
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 flex-grow-0 gap-8">
+          <div>meuh</div>
+        </div>
+      </CenterAlign>
+    </div>
+  )
+}
