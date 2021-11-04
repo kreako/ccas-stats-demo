8172836d60b8e15894346baf52d89ef20525c9e7
kreako <olivier@kreako.fr>
2021-11-04 21:03:02 +0100

    Add a GenderPie to present gender results

--- src/features/stat/Stat.tsx
+++ src/features/stat/Stat.tsx
@@ -1,26 +1,32 @@
 import { ResponsiveCalendar } from "@nivo/calendar"
-import { sky } from "tailwindcss/colors"
+import { ResponsivePie } from "@nivo/pie"
+import { blue, blueGray, pink, sky } from "tailwindcss/colors"
 import { useAppSelector } from "../../app/hooks"
-import { selectEventCountPerDate } from "../event/eventSlice"
+import {
+  selectEventCountPerDate,
+  selectEventGenderPerDate,
+} from "../event/eventSlice"
 
-type EventCalendarProps = {
+type StatsProps = {
   label: string
   from: string
   to: string
 }
 
-function EventCalendar({ label, from, to }: EventCalendarProps) {
+function EventCalendar({ label, from, to }: StatsProps) {
   const data = useAppSelector((state) =>
     selectEventCountPerDate(state, from, to)
   )
   return (
-    <div className="flex flex-col space-y-1 items-center h-48 lg:h-64 xl:h-96">
-      <ResponsiveCalendar
-        data={data}
-        from={from}
-        to={to}
-        colors={[sky[200], sky[400], sky[600], sky[800]]}
-      />
+    <div className="flex flex-col space-y-1 items-center">
+      <div className="flex-grow w-full h-48 lg:h-64 xl:h-96">
+        <ResponsiveCalendar
+          data={data}
+          from={from}
+          to={to}
+          colors={[sky[200], sky[400], sky[600], sky[800]]}
+        />
+      </div>
       <div className="flex flex-col items-center">
         <div className="text-sm text-blueGray-700">{label}</div>
         <div className="text-xs text-blueGray-700">
@@ -34,20 +40,64 @@ function EventCalendar({ label, from, to }: EventCalendarProps) {
   )
 }
 
+function GenderPie({ label, from, to }: StatsProps) {
+  const count = useAppSelector((state) =>
+    selectEventGenderPerDate(state, from, to)
+  )
+  const data = [
+    {
+      id: "Femme",
+      value: count.female,
+    },
+    {
+      id: "Homme",
+      value: count.male,
+    },
+    {
+      id: "Autre",
+      value: count.other,
+    },
+  ]
+  return (
+    <div className="flex flex-col space-y-4 items-center">
+      <div className="flex-grow w-full h-64 lg:h-64 xl:h-96">
+        <ResponsivePie
+          data={data}
+          colors={[pink[300], sky[300], blueGray[300]]}
+          borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
+          innerRadius={0.3}
+          padAngle={0.7}
+          cornerRadius={0.3}
+          arcLinkLabelsColor={{ from: "color" }}
+          arcLinkLabelsSkipAngle={10}
+          arcLinkLabelsTextColor={{ from: "color", modifiers: [["darker", 3]] }}
+          arcLinkLabelsThickness={2}
+          arcLabelsTextColor={{ from: "color", modifiers: [["darker", 3]] }}
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="text-sm text-blueGray-700">{label}</div>
+        <div className="text-xs text-blueGray-700">Répartition par genre</div>
+      </div>
+    </div>
+  )
+}
+
 export default function StatPage() {
   return (
     <div className="mt-8 w-full">
       <div className="uppercase tracking-wider text-xs font-bold text-sky-900 ml-2 md:ml-4 lg:ml-8 xl:ml-16">
         Calendrier
       </div>
-      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 flex-grow-0 gap-8 px-1">
-        <div className="col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4">
+      <div className="mt-4 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 flex-grow-0 gap-x-8 gap-y-16 px-1">
+        <div className="col-span-1 lg:col-span-2 xl:col-span-3">
           <EventCalendar
             label="Visites 2021"
             from="2021-01-01"
             to="2021-12-31"
           />
         </div>
+        <GenderPie label="Visites 2021" from="2021-01-01" to="2021-12-31" />
       </div>
     </div>
   )
